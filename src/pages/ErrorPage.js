f46435a48872d8bd8156a54d04f36a7485b10f23
Ronkl14 <121381849+Ronkl14@users.noreply.github.com>
2023-02-26 08:57:07 +0200

    changed title

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,11 +1,16 @@
 import React from "react";
 import notFound from "../assets/img/pulp-fiction-john-travolta.gif";
+import { HomeStyled } from "../styled";
+import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
   return (
-    <div>
+    <HomeStyled>
       <img src={notFound} alt="not found" />
-    </div>
+      <h2>
+        Can't find what you're looking for buddy <Link to="/">Back Home</Link>
+      </h2>
+    </HomeStyled>
   );
 };
 
